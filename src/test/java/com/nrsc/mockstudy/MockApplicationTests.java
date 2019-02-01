0dd89a5cabc1b8d6a0ca0ce57a7d0dc4d7137abe
sunchuan <sunchuan-ds@gome.com.cn>
2019-02-01 12:24:54 +0800

    将MockApplicationTests.java注释掉

--- src/test/java/com/nrsc/mockstudy/MockApplicationTests.java
+++ src/test/java/com/nrsc/mockstudy/MockApplicationTests.java
@@ -1,17 +1,17 @@
-package com.nrsc.mockstudy;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MockApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//package com.nrsc.mockstudy;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class MockApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
 
